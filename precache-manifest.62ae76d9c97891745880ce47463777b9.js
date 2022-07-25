9258b7d3f22152853c1fe8c19f4d20b5b9a75e19
Sanskar Gubreley <gubreley789@gmail.com>
2022-07-25 10:26:43 +0530

    Updates

---
+++ precache-manifest.62ae76d9c97891745880ce47463777b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "150b9387777073c6e30c",
+    "url": "/TextUtils-React-Application/static/css/main.048d182f.chunk.css"
+  },
+  {
+    "revision": "150b9387777073c6e30c",
+    "url": "/TextUtils-React-Application/static/js/main.150b9387.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/TextUtils-React-Application/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React-Application/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "668a2cbeefa99d7ee90f",
+    "url": "/TextUtils-React-Application/static/js/runtime~main.668a2cbe.js"
+  },
+  {
+    "revision": "00fd5991cee5e7fb1e69524f4be25a2d",
+    "url": "/TextUtils-React-Application/index.html"
+  }
+];
